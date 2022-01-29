e200a31a6208900c866a9b67d9595f0c39cfddfa
Вероника <l.antonenko2017@yandex.ru>
2022-01-29 13:44:52 +0300

    Test

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
-from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+from PyQt5.QtGui import QPainter
+import random
 from PyQt5 import uic
-from random import randint
 
 
 class Test(QMainWindow):
@@ -10,31 +9,8 @@ class Test(QMainWindow):
         super().__init__()
         uic.loadUi('UI.ui', self)
 
-        self.pushButton.setFixedSize(100, 100)
-        self.pushButton.clicked.connect(self.circle)
 
-        self.label = QLabel()
-        canvas = QPixmap(600, 600)
-        self.label.setPixmap(canvas)
-
-        layout = QGridLayout(self.centralwidget)
-        layout.addWidget(self.pushButton, 0, 0, alignment=Qt.AlignCenter)
-        layout.addWidget(self.label, 1, 0)
-
-    def circle(self):
-        x, y = [randint(10, 500) for i in range(2)]
-        w, h = [randint(10, 100) for i in range(2)]
-        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
-        painter = QPainter(self.label.pixmap())
-        pen = QPen()
-        pen.setWidth(3)
-        pen.setColor(QColor(*[randint(0, 255) for i in range(3)]))
-        painter.setPen(pen)
-        painter.drawEllipse(x, y, w, h)
-        painter.end()
-        self.update()
-
-
+        
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
