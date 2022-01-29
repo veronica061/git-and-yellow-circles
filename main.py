40dd0d171603880954b35d52abd68079431d4fbc
Вероника <l.antonenko2017@yandex.ru>
2022-01-29 13:55:03 +0300

    Final commit

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
-from PyQt5.QtGui import QPainter
-import random
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLabel, QGridLayout
+from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
+from random import randint 
 
 
 class Test(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.circle)
+        canvas = QPixmap(600, 600)
+        self.label.setPixmap(canvas)
 
+    def circle(self):
+        x, y = [randint(10, 500) for i in range(2)]
+        w, h = [randint(10, 100) for i in range(2)] 
+        painter = QPainter(self.label.pixmap())  
+        painter.setBrush(QColor('yellow'))
+        painter.drawEllipse(x, y, w, h)
+        painter.end()
+        self.update()
 
-        
+    
 if __name__ == '__main__':
     import sys
     app = QApplication(sys.argv)
